97c9a463395ab07776d281036a17eecc56acf8ba
Shubhjn4357 <shubhamjain.com.in@gmail.com>
2026-02-14 19:33:19 +0530

    feat: Initialize Hono server with logger middleware in `server.ts`.

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
-import { serve } from '@hono/node-server';
-import app from './app.js';
-import * as dotenv from 'dotenv'
-import { logger } from 'hono/logger';
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config();
-}
-const port = Number(process.env.PORT || 3000);
-app.use(logger())
-console.log(`🚀 Server starting on ${port}`);
 
-serve({
-    fetch: app.fetch,
-    port,
-});
-export const runtime = 'edge';
+import app from './app';
+
+import { logger } from 'hono/logger';
+app.use(logger())
+export const GET = app;
+export const POST = app;
+export const PUT = app;
+export const DELETE = app;
+export const PATCH = app;
+export default app;
